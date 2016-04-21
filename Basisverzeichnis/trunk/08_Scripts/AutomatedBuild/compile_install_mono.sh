e56ce483c353fb9a799313c20773fc2a33a01d38
SPoelloth <stefanpoelloth@yahoo.de>
2016-04-21 11:21:45 +0200

    Mono 4.4.0 Installationsscript hinzugefügt #10
    Mono Installationsscript: Läd Verson 4.4.0.122 von Mono herunter,
    compiliert und installiert diese.
    Buildscript umbenannt.

---
+++ Basisverzeichnis/trunk/08_Scripts/AutomatedBuild/compile_install_mono.sh
@@ -0,0 +1,20 @@
+# alles das für den build für mono gebraucht wird installieren
+sudo apt-get update
+sudo apt-get install build-essential autoconf automake curl binutils libtool libglib2.0-dev libxrender-dev libfontconfig1-dev libpng12-dev libgif-dev libjpeg8-dev libtiff5-dev libexif-dev gettext libcairo2-dev libgdiplus
+
+# ordner anlegen, unsere zielversion runterladen und entpacken
+# unsere zielversion ist 4.4.0.122
+# andere versionen gibt es hier: http://download.mono-project.com/sources/mono/
+
+mkdir sources
+cd sources
+wget http://download.mono-project.com/sources/mono/mono-4.4.0.122.tar.bz2
+tar -jxvf mono-4.4.0.122.tar.bz2
+
+# mono compilieren
+cd mono-4.4.0
+./configure --prefix=/usr/local/
+sudo make -j8
+
+#mono installieren
+sudo make install
