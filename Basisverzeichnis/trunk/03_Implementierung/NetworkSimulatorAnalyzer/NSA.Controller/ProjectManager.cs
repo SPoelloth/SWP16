ce0711196974f81ef0e3623e509fa094fe4bc950
gitJonHaw <jon.koenig@oth-aw.de>
2016-05-25 13:53:30 +0200

    Erste Versuche am ProjectManager

--- Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Controller/ProjectManager.cs
+++ Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Controller/ProjectManager.cs
@@ -1,45 +1,70 @@
-using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
 using NSA.Model.BusinessLogic;
 
 namespace NSA.Controller
 {
     internal class ProjectManager
     {
-        public Project CurrentProject { get; }
-        public List<Testscenario> Testscenarios { get; }
+        private Project _currentProject;
+        private List<Testscenario> _testscenarios;
 
         // Default constructor:
         public ProjectManager()
         {
-
+            _currentProject = null;
         }
 
         // Constructor:
         public ProjectManager(Project currentProject, List<Testscenario> testscenarios)
         {
-            CurrentProject = currentProject;
-            Testscenarios = testscenarios;
+            _currentProject = currentProject;
+            _testscenarios = testscenarios;
         }
 
         public void CreateNewProject()
         {
-
+            _currentProject = new Project();
         }
 
         public void CloseProject()
         {
-
+            _currentProject = null;
         }
 
         public void SaveAs(string path)
         {
-
+            var file = new StreamWriter(path);
+            file.WriteLine(_currentProject);
+            file.Close();
         }
 
         public void LoadTestscenarios()
         {
+            var openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            var file = openFileDialog.FileName;
+            try
+            {
+                _testscenarios.Add(new Testscenario());
+            }
+            catch (IOException)
+            {
+            }
+        }
 
+        public Testscenario GetTestscenarioById(string id)
+        {
+            foreach (var testscenario in _testscenarios)
+            {
+                if (testscenario.Id.Equals(id))
+                {
+                    return testscenario;
+                }
+            }
+            return null;
         }
     }
 }
--- Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/BusinessLogic/Testscenario.cs
+++ Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/BusinessLogic/Testscenario.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace NSA.Model.BusinessLogic
 {
 	public class Testscenario
     {
         private List<Rule> rules;
+
+        public Testscenario()
+        {
+        }
+
+        public Testscenario(string id)
+	    {
+	        Id = id;
+	    }
+
+	    public string Id { get; }
     }
 }
