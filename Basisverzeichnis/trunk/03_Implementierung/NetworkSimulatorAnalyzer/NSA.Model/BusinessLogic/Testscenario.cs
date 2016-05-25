03fdf695c9cb60822039983bee2abb241049a4b9
gitJonHaw <jon.koenig@oth-aw.de>
2016-05-25 14:35:32 +0200

    Kommentar für Testscenario-Ersteller: Testcenario braucht eine id (wurde hinzugefügt)

--- Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/BusinessLogic/Testscenario.cs
+++ Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/BusinessLogic/Testscenario.cs
@@ -10,7 +10,8 @@ namespace NSA.Model.BusinessLogic
         public Testscenario()
         {
         }
-
+        // todo: an id is needed
+        // in the ProjectManager is a method to load the testscenario by id so the testscenario has to have an id
         public Testscenario(string id)
 	    {
 	        Id = id;
