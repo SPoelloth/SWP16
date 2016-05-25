45befde7907c3c3b5e5769bb79dfaf144ceb675d
gitJonHaw <jon.koenig@oth-aw.de>
2016-05-25 14:21:18 +0200

    Der Projectmanager ist vorerst fertig.

--- Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Controller/ProjectManager.cs
+++ Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Controller/ProjectManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using NSA.Model.BusinessLogic;
 
@@ -26,18 +27,18 @@ namespace NSA.Controller
         public void CreateNewProject()
         {
             _currentProject = new Project();
+            _testscenarios = new List<Testscenario>();
         }
 
         public void CloseProject()
         {
             _currentProject = null;
+            _testscenarios.Clear();
         }
 
         public void SaveAs(string path)
         {
-            var file = new StreamWriter(path);
-            file.WriteLine(_currentProject);
-            file.Close();
+            WriteToBinaryFile(path, _currentProject);
         }
 
         public void LoadTestscenarios()
@@ -48,7 +49,7 @@ namespace NSA.Controller
             var file = openFileDialog.FileName;
             try
             {
-                _testscenarios.Add(new Testscenario());
+                _testscenarios.Add(ReadFromBinaryFile<Testscenario>(file));
             }
             catch (IOException)
             {
@@ -57,14 +58,40 @@ namespace NSA.Controller
 
         public Testscenario GetTestscenarioById(string id)
         {
-            foreach (var testscenario in _testscenarios)
+            return _testscenarios?.FirstOrDefault(testscenario => testscenario.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Writes the given object instance to a binary file.
+        /// <para>Object type (and all child types) must be decorated with the [Serializable] attribute.</para>
+        /// <para>To prevent a variable from being serialized, decorate it with the [NonSerialized] attribute; cannot be applied to properties.</para>
+        /// </summary>
+        /// <typeparam name="T">The type of object being written to the XML file.</typeparam>
+        /// <param name="filePath">The file path to write the object instance to.</param>
+        /// <param name="objectToWrite">The object instance to write to the XML file.</param>
+        /// <param name="append">If false the file will be overwritten if it already exists. If true the contents will be appended to the file.</param>
+        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
             {
-                if (testscenario.Id.Equals(id))
-                {
-                    return testscenario;
-                }
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
             }
-            return null;
+        }
+
+        /// <summary>
+        /// Reads an object instance from a binary file.
+        /// </summary>
+        /// <typeparam name="T">The type of object to read from the XML.</typeparam>
+        /// <param name="filePath">The file path to read the object instance from.</param>
+        /// <returns>Returns a new instance of the object read from the binary file.</returns>
+        public static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
         }
     }
 }
