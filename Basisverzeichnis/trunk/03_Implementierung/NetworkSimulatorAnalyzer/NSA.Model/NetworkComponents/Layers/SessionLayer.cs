2d0e603f17637321040cdc24c64645081a3d2365
ReVoluti0n <jp.jeremy.probst@gmail.com>
2016-06-13 13:32:06 +0200

    #23 Layers public, fixes

--- Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/NetworkComponents/Layers/SessionLayer.cs
+++ Basisverzeichnis/trunk/03_Implementierung/NetworkSimulatorAnalyzer/NSA.Model/NetworkComponents/Layers/SessionLayer.cs
@@ -4,7 +4,7 @@ using NSA.Model.NetworkComponents.Helper_Classes;
 
 namespace NSA.Model.NetworkComponents.Layers
 {
-    class SessionLayer : ILayer
+    public class SessionLayer : ILayer
     {
         public bool ValidateReceive(IPAddress nextNodeIP, Workstation currentNode, Result Res)
         {
